85097ee7369d49d642a3dd6faba5de213fe72ccc
Isfak-Shakil <isfakshakil@gmail.com>
2021-07-04 14:20:49 +0600

    Google SignIn Authentication

--- app/src/main/java/com/example/mycontactlist/reository/SignInRepo.java
+++ app/src/main/java/com/example/mycontactlist/reository/SignInRepo.java
@@ -1,15 +1,22 @@
 package com.example.mycontactlist.reository;
 
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.lifecycle.MutableLiveData;
 
 import com.example.mycontactlist.model.SignInUser;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class SignInRepo {
     private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
     private SignInUser user = new SignInUser();
-
+// check in login  or not
     public MutableLiveData<SignInUser> checkAuthenticationInFirebase(){
         MutableLiveData<SignInUser> isAuthenticateLiveData =new MutableLiveData<>();
 
@@ -25,5 +32,23 @@ public class SignInRepo {
         }
 
         return isAuthenticateLiveData;
+    }
+    // sign in user
+    public MutableLiveData<String>  firebaseSignInGoogle(AuthCredential authCredential){
+        MutableLiveData<String> authMutableLiveData=new MutableLiveData<>();
+        firebaseAuth.signInWithCredential(authCredential).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+        FirebaseUser currentUser=firebaseAuth.getCurrentUser();
+        String uId=currentUser.getUid();
+        authMutableLiveData.setValue(uId);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+              authMutableLiveData.setValue(e.toString());
+            }
+        });
+        return authMutableLiveData;
     }
 }
--- app/src/main/java/com/example/mycontactlist/viewModel/SignInViewModel.java
+++ app/src/main/java/com/example/mycontactlist/viewModel/SignInViewModel.java
@@ -9,14 +9,19 @@ import androidx.lifecycle.MutableLiveData;
 
 import com.example.mycontactlist.model.SignInUser;
 import com.example.mycontactlist.reository.SignInRepo;
+import com.google.firebase.auth.AuthCredential;
 
 public class SignInViewModel extends AndroidViewModel {
    private SignInRepo repository;
    public LiveData<SignInUser> checkAuthenticateLiveData;
+   public LiveData<String> authenticateLiveData;
     public SignInViewModel(@NonNull Application application) {
         super(application);
 
         repository=new SignInRepo();
+    }
+    public   void  signInWithGoogle(AuthCredential authCredential){
+        authenticateLiveData=repository.firebaseSignInGoogle(authCredential);
     }
     public void checkAuthenticate(){
         checkAuthenticateLiveData=repository.checkAuthenticationInFirebase();
